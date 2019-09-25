f8668dc69eb199b7a017a45c940c145e277641bc
Tyler Rouze <rouze012@umn.edu>
2019-09-24 20:35:34 -0500

    Updates

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,9 +3,4 @@ var store = [{
         "excerpt":"Hi all, this is the first post on this site.  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/journal/myfirstpost",
-        "teaser":"http://localhost:4000/15940374_1373164969373729_1797987871538647164_n.jpg"},{
-        "title": "My Second Post",
-        "excerpt":"Hi all, this is the second post on this site.  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/journal/mysecondpost",
         "teaser":"http://localhost:4000/15940374_1373164969373729_1797987871538647164_n.jpg"}]
